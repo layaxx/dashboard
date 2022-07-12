b8245907f0db851c85eec71750008c3547ef759b
layaxx <layaxx@gmx.de>
2022-07-12 21:33:34 +0200

    add seeds for dev environment

--- db/seeds.ts
+++ db/seeds.ts
@@ -1,16 +1,48 @@
-// import db from "./index"
+import { SecurePassword } from "blitz"
+import { faker } from "@faker-js/faker"
+import db from "db"
 
-/*
- * This seed function is executed when you run `blitz db seed`.
- *
- * Probably you want to use a library like https://chancejs.com
- * or https://github.com/Marak/Faker.js to easily generate
- * realistic data.
- */
 const seed = async () => {
-  // for (let i = 0; i < 5; i++) {
-  //   await db.project.create({ data: { name: "Project " + i } })
-  // }
+  await db.$reset()
+
+  await db.user.create({
+    data: {
+      email: "blitz@localhost.localdomain",
+      name: "Local Account",
+      role: "ADMIN",
+      hashedPassword: await SecurePassword.hash("password"),
+    },
+  })
+
+  const numberOfFeeds = 5
+  const numberOfEntriesPerFeed = 10
+
+  for (let index = 0; index < numberOfFeeds; index++) {
+    const { id } = await db.feed.create({
+      data: {
+        name: faker.internet.userName(),
+        position: index,
+        loadIntervall: 15,
+        url: faker.internet.url(),
+        lastLoad: faker.date.past(),
+      },
+    })
+
+    for (let indexEntry = 0; indexEntry < numberOfEntriesPerFeed; indexEntry++) {
+      await db.feedentry.create({
+        data: {
+          id: faker.unique(faker.datatype.string),
+          link: faker.internet.url(),
+          summary: faker.lorem.paragraph(),
+          title: faker.lorem.sentence(),
+          text: faker.lorem.paragraphs(3),
+          feedId: id,
+        },
+      })
+    }
+
+    await db.readlistentry.create({ data: { url: faker.internet.url() } })
+  }
 }
 
 export default seed
