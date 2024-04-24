b3956749a483cdc6bc54dafdad87fc62fb8882d7
layaxx <layaxx@gmx.de>
2024-04-24 12:27:20 +0200

    make main layout work better on wide screens

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -20,32 +20,34 @@ const Layout: BlitzLayout<{ title?: string; heading: string; children: React.Rea
       </Head>
 
       <header className={clsx("bg-slate-600", "p-12", "text-gray-200")}>
-        <h1
-          className={clsx(
-            "font-extrabold",
-            "text-4xl",
-            "sm:text-5xl",
-            "md:text-6xl",
-            "text-gray-100",
-            "tracking-tight"
-          )}
-        >
-          <span className={clsx("block", "xl:inline")}>{heading}</span>
-        </h1>
-        <nav className="mt-4">
-          {router.pathname
-            .split("/")
-            .filter((element, index) => index === 0 || element)
-            .map((element, index, array) => {
-              const path = array.slice(0, index + 1).join("/")
-              return (
-                <Fragment key={path}>
-                  <Link href={path || "/"}>{element || "Home"}</Link>
-                  <span className="mx-2">/</span>
-                </Fragment>
-              )
-            })}
-        </nav>
+        <div className={clsx("max-w-screen-2xl", "mx-auto")}>
+          <h1
+            className={clsx(
+              "font-extrabold",
+              "text-4xl",
+              "sm:text-5xl",
+              "md:text-6xl",
+              "text-gray-100",
+              "tracking-tight"
+            )}
+          >
+            <span className={clsx("block", "xl:inline")}>{heading}</span>
+          </h1>
+          <nav className="mt-4">
+            {router.pathname
+              .split("/")
+              .filter((element, index) => index === 0 || element)
+              .map((element, index, array) => {
+                const path = array.slice(0, index + 1).join("/")
+                return (
+                  <Fragment key={path}>
+                    <Link href={path || "/"}>{element || "Home"}</Link>
+                    <span className="mx-2">/</span>
+                  </Fragment>
+                )
+              })}
+          </nav>
+        </div>
       </header>
       <main className={clsx("flex", "flex-wrap", "justify-around", "m-auto", "p-8")}>
         {children}
