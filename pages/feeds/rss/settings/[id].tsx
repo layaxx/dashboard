e67ae4f0fd46dc1e98bb60698d875aaa53b5a33f
layaxx <layaxx@gmx.de>
2024-10-01 21:57:00 +0200

    refactor(layouts): add CustomErrorComponent to CustomErrorFallback

--- pages/feeds/rss/settings/[id].tsx
+++ pages/feeds/rss/settings/[id].tsx
@@ -1,24 +1,41 @@
-import { Suspense } from "react"
-import { BlitzPage, useParam } from "@blitzjs/next"
+import React from "react"
+import { BlitzPage } from "@blitzjs/next"
 import Head from "next/head"
+import { gSSP } from "app/blitz-server"
 import Layout from "app/core/layouts/Layout"
 import SettingsForm from "app/feeds/components/settings/Form"
-import SettingsFormSkeleton from "app/feeds/components/settings/FormSkeleton"
+import getFeed from "app/feeds/queries/getFeed"
+import { Feed } from "db"
 
-const FeedsSettingsPage: BlitzPage = () => {
-  const id = useParam("id")
-
+const FeedsSettingsPage: BlitzPage<{ feed: Feed }> = ({ feed }) => {
   return (
     <>
       <Head>
         <title>Feeds - Settings</title>
       </Head>
-      <Suspense fallback={<SettingsFormSkeleton />}>
-        <SettingsForm id={Number(id)} />
-      </Suspense>
+
+      <SettingsForm feed={feed} />
     </>
   )
 }
+
+export const getServerSideProps = gSSP<{ feed?: Feed }>(async ({ ctx, params }) => {
+  let feed: Feed
+  try {
+    feed = await getFeed({ id: Number(params?.id) }, ctx)
+  } catch (error) {
+    console.error(error)
+    return {
+      props: {},
+    }
+  }
+
+  return {
+    props: {
+      feed,
+    },
+  }
+})
 
 FeedsSettingsPage.authenticate = true
 FeedsSettingsPage.getLayout = (page) => <Layout heading="RSS Settings Page">{page}</Layout>
