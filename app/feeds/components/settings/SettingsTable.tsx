64f700686a4256fdaa82743d2dc5c59b662b8674
layaxx <layaxx@gmx.de>
2024-11-14 16:53:07 +0100

    fix: prevent overflow in feed settings table

--- app/feeds/components/settings/SettingsTable.tsx
+++ app/feeds/components/settings/SettingsTable.tsx
@@ -3,12 +3,14 @@ import clsx from "clsx"
 
 const SettingsTable: React.FC<{ rows: [string, ReactNode][] }> = ({ rows }) => {
   return (
-    <table className={clsx("border", "border-collapse", "border-gray-200", "w-full")}>
+    <table
+      className={clsx("border", "border-collapse", "border-gray-200", "table-fixed", "w-full")}
+    >
       <tbody>
         {rows.map(([name, value]) => (
           <tr className={clsx("border-b", "border-gray-200")} key={name}>
-            <td className={clsx("border-gray-200", "border-r", "p-2", "w-1/2")}>{name}</td>
-            <td className="p-2">{value}</td>
+            <td className={clsx("border-gray-200", "border-r", "p-2", "w-1/3")}>{name}</td>
+            <td className={clsx("break-words", "p-2")}>{value}</td>
           </tr>
         ))}
       </tbody>
