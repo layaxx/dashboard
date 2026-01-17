c03f0352992bb173cd6caf40c96e3c0b20fc295f
layaxx <layaxx@gmx.de>
2026-01-17 14:33:53 +0100

    fix: add seed API route for DEMO mode

--- pages/api/demo/seed.ts
+++ pages/api/demo/seed.ts
@@ -2,18 +2,26 @@ import { BlitzLogger } from "blitz"
 import { getSession } from "@blitzjs/auth"
 import { NextApiRequest, NextApiResponse } from "next"
 import { api } from "app/blitz-server"
-import db from "db"
+import seed from "db/seeds"
 
-const logger = BlitzLogger({ name: "/api/unarchive-all" })
+const logger = BlitzLogger({ name: "/api/demo/seed" })
 
 const handler = async (request: NextApiRequest, response: NextApiResponse) => {
   const session = await getSession(request, response)
 
+  if (process.env.NEXT_PUBLIC_IS_DEMO_MODE !== "true") {
+    logger.warn("/api/demo/seed called but demo mode is not active")
+    response.statusCode = 403
+    response.statusMessage = "Demo mode is not active"
+    response.end()
+    return
+  }
+
   if (!session.userId) {
     if (process.env.API_TOKEN && request.headers["api-token"] === process.env.API_TOKEN) {
-      logger.info("Access to /api/unarchive-all granted due to valid api token")
+      logger.info("Access to /api/demo/seed granted due to valid api token")
     } else {
-      logger.warn("Access to /api/unarchive-all denied")
+      logger.warn("Access to /api/demo/seed denied")
       response.statusCode = 403
       response.statusMessage = "Please log in to use this API route"
       response.end()
@@ -22,21 +30,13 @@ const handler = async (request: NextApiRequest, response: NextApiResponse) => {
   }
 
   try {
-    const result = await db.feedentry
-      .updateMany({
-        where: { isArchived: true },
-        data: { isArchived: false },
-      })
-      .then(({ count }) => {
-        logger.info(`Unarchived ${count} feed entries`)
-        return count
-      })
+    await seed()
 
     response.statusCode = 200
-    response.end(`Unarchived ${result} entries`)
+    response.end(`Seeded demo data successfully`)
   } catch {
     response.statusCode = 500
-    response.end(`An error occurred during unarchiving entries`)
+    response.end(`An error occurred during seeding demo data`)
   }
 }
 export default api(handler)
