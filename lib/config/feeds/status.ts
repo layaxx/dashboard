1b88ea6d70f930614610415cb600b4a0734e4711
layaxx <layaxx@gmx.de>
2026-02-12 17:23:53 +0100

    fix: correct usage of process.env for loading environment variable in status.ts

--- lib/config/feeds/status.ts
+++ lib/config/feeds/status.ts
@@ -1,8 +1,8 @@
-import { env } from "process"
+import process from "process"
 
 export const maxAcceptableTimeBetweenLoads = 61
 export const maxAcceptableAverageLoadTime = 500
 export const targetTimeBetweenLoads = Number.parseInt(
-  env.NEXT_PUBLIC_TARGET_TIME_BETWEEN_LOADS || "10",
+  process.env.NEXT_PUBLIC_TARGET_TIME_BETWEEN_LOADS || "10",
   10
 )
--- next.config.js
+++ next.config.js
@@ -13,9 +13,5 @@ const config = {
     ]
   },
   output: "standalone",
-  env: {
-    NEXT_PUBLIC_TARGET_TIME_BETWEEN_LOADS:
-      process.env.NEXT_PUBLIC_TARGET_TIME_BETWEEN_LOADS || "10",
-  },
 }
 module.exports = withBlitz(config)
